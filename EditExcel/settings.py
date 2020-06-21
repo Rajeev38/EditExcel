495246b25dc7d2f37984404dc63e138a5e6f8962
Rajeev Kushwaha <rajeevkushwaha38@gmail.com>
2020-06-21 12:10:10 +0530

    Error correction for static  files

--- EditExcel/settings.py
+++ EditExcel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_n#m1psg^kl#@p4)z=!672=s4n=sy9%+wk%rm*e7z^^65#&7@y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
